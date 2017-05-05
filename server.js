a7cdf1b29651bf0f37307d10b1f76766e74fc6bd
sunjieming <ben@lambdaschool.com>
2017-05-04 20:23:41 -0600

    Add new-movie route

--- server.js
+++ server.js
@@ -9,18 +9,21 @@ app.use(CORS());
 
 const movies = [
 	{
+		id: 0,
 		title: 'The Godfather',
 		director: 'Francis Ford Coppola',
 		metascore: 100,
 		stars: ['Marlon Brando', 'Al Pacino', 'Robert Duvall'],
 	},
 	{
+		id: 1,
 		title: 'Star Wars',
 		director: 'George Lucas',
 		metascore: 92,
 		stars: ['Mark Hamill', 'Harrison Ford', 'Carrie Fisher'],
 	},
 	{
+		id: 2,
 		title: 'The Lord of the Rings: The Fellowship of the Ring',
 		director: 'Peter Jackson',
 		metascore: 92,
@@ -35,6 +38,11 @@ app.get('/movies', (req, res) => {
 app.get('/movies/:id', (req, res) => {
 	const movie = movies.filter(movie => movie.id === req.params.id)[0];
 	res.send(movie);
+});
+
+app.post('/new-movie', (req, res) => {
+	if (req.body.id !== undefined) movies.push(req.body);
+	res.send(movies);
 });
 
 app.listen(5000, () => {
