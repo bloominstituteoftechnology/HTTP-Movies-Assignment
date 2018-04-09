aee99c6db6f74a971ca45f53d3443aea0e5d8114
Ryan Hamblin <ryanhca@gmail.com>
2018-04-08 21:45:17 -0600

    adds reminder to stretch commented out code

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -25,7 +25,7 @@ export default class Movie extends Component {
         console.error(error);
       });
   };
-
+  // Uncomment this code when you're ready for the stretch problems
   // componentWillReceiveProps(newProps){
   //   if(this.props.match.params.id !== newProps.match.params.id){
   //     this.fetchMovie(newProps.match.params.id);
