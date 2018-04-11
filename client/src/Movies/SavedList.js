1324b20a9697bc5b1a0f2df01ffa5768b16b77b3
Ryan Hamblin <ryanhca@users.noreply.github.com>
2018-04-11 09:45:17 -0600

    Update SavedList.js

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react';
-
+import React, { Component } from 'react'
+import { NavLink, Link } from 'react-router-dom';
 export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
+  constructor(props){
+    super(props)
   }
 
-  render() {
+  render(){
     return (
       <div className="saved-list">
-        <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
-        ))}
-        <div className="home-button">Home</div>
+        <h3 >Saved Movies:</h3>
+        {this.props.list.map( movie => {
+          return (
+            <NavLink to={`/movies/${movie.id}`} key={movie.id} activeClassName="saved-active">
+              <span className="saved-movie">{movie.title}</span>
+            </NavLink>
+          )
+        })}
+        <div className="home-button">
+          <Link to="/">Home</Link>
+        </div>
       </div>
-    );
+    )
   }
 }
