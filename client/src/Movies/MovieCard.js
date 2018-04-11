4bca04d21e1255d772bc39e34c2cc30457e2b03d
Ryan Hamblin <ryanhca@users.noreply.github.com>
2018-04-11 09:44:42 -0600

    Update MovieCard.js

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from 'react'
 
-const MovieCard = props => {
-  return;
-};
+const MovieCard = (props) => {
+  const {title, director, metascore, stars} = props.movie;
+  return (
+    <div className="movie-card">
+      <h2>{title}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
 
-export default MovieCard;
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default MovieCard
