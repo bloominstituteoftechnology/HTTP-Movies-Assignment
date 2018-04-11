a06ab37522b760bdbbbf30a0adbd147bdf81bae5
Ryan Hamblin <ryanhca@users.noreply.github.com>
2018-04-11 09:45:38 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,33 @@
 import React, { Component } from 'react';
-
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import Movie from './Movies/Movie'
 
 export default class App extends Component {
-  constructor() {
-    super();
+  constructor(){
+    super()
     this.state = {
       savedList: []
-    };
+    }
   }
 
-  addToSavedList = movie => {
+  addToSavedList = (movie) => {
+    console.log(this.state.savedList)
     const savedList = this.state.savedList;
     savedList.push(movie);
-    this.setState({ savedList });
-  };
+    this.setState({savedList});
+  }
 
-  render() {
+  render(){
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" render={ (props) => {
+          return(<Movie {...props} addToSavedList={this.addToSavedList}/>)
+        }} />
       </div>
-    );
+    )
   }
 }
