156c972bf486f7280f05464d116958068a05dcf5
Ryan Hamblin <ryanhca@users.noreply.github.com>
2018-04-11 09:44:59 -0600

    Update MovieList.js

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
@@ -10,14 +11,8 @@ export default class MovieList extends Component {
   }
 
   componentDidMount() {
-    axios
-      .get('http://localhost:5000/api/movies')
-      .then(response => {
-        this.setState(() => ({ movies: response.data }));
-      })
-      .catch(error => {
-        console.error('Server Error', error);
-      });
+    // fill me in with an HTTP Request to `localhost:5000/api/movies`
+    this.setState({ movies: [] });
   }
 
   render() {
@@ -32,23 +27,9 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
+    <Link to={`/movies/${movie.id}`}>
+      <MovieCard movie={movie} />
+    </Link>
   );
 }
