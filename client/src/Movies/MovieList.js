ed968ea82e24e14bdb2d994a77f0475e9a7baaa5
Yasin Shuman <email@yasin.io>
2018-04-12 16:45:52 -0700

    completed part 1

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import MovieCard from './MovieCard';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import MovieCard from "./MovieCard";
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
@@ -12,6 +12,14 @@ export default class MovieList extends Component {
 
   componentDidMount() {
     // fill me in with an HTTP Request to `localhost:5000/api/movies`
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then(res => {
+        this.setState({ movies: res.data });
+      })
+      .catch(err => {
+        console.log("something went wrong!!! here is the error", err);
+      });
     this.setState({ movies: [] });
   }
 
