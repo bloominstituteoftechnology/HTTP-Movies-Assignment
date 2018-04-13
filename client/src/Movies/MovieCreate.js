29d5bde5e21feff9722c15c33fc5c29496750b85
Yasin Shuman <email@yasin.io>
2018-04-12 19:04:28 -0700

    added axios, not sure how to handle actors. getting error about cannot read property map of undefined dealing with stars on line 16 of moviecard.js

--- client/src/Movies/MovieCreate.js
+++ client/src/Movies/MovieCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class MovieCreate extends Component {
   state = {
@@ -8,36 +9,63 @@ export default class MovieCreate extends Component {
     stars: []
   };
 
+  handleInput = e => {
+    console.log(e.target.value);
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = () => {
+    const { title, director, metascore, stars } = this.state;
+    const newMovie = { title, director, metascore, stars };
+    axios
+      .post("http://localhost:5000/api/movies", newMovie)
+      .then(res => {
+        console.log("this is the response from the post", res);
+        this.props.history.push("/");
+      })
+      .catch(err => {
+        console.log("something went wrong on the post request", err);
+      });
+  };
+
   render() {
-    
     return (
       <div>
         <input
           type="text"
           placeholder="Title"
           value={this.state.title}
-          onChange={}
+          onChange={this.handleInput}
+          name="title"
         />
         <input
           type="text"
-          placeholder="Title"
-          value={this.state.title}
-          onChange={}
+          placeholder="Director"
+          value={this.state.director}
+          onChange={this.handleInput}
+          name="director"
         />
         <input
           type="text"
-          placeholder="Title"
-          value={this.state.title}
-          onChange={}
+          placeholder="Metascore"
+          value={this.state.metascore}
+          onChange={this.handleInput}
+          name="metascore"
         />
-        <input
+        {/* i dont know how to add more than one actor?
+         do i make a function that adds to the array? if so, how do i call it multiple times?
+         do we expect the user to put them in all at once?
+        */}
+        {/* <input
           type="text"
-          placeholder="Title"
-          value={this.state.title}
-          onChange={}
-        />
+          placeholder="Actor"
+          value={this.state.actor}
+          onChange={this.handleInput}
+          name="actor"
+        /> */}
+
+        <button onClick={this.handleSubmit}>Add Movie</button>
       </div>
-      
-    )
+    );
   }
 }
