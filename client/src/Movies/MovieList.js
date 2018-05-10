b6547661ae2d76b7a897cfcd02f30da33625e707
Brandon Benefield <bsquared18@gmail.com>
2018-05-10 15:58:48 -0400

    feat(MovieList): correctly grabs data from `http://localhost:5000/api/movies` and displays it in the component

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -12,7 +12,8 @@ export default class MovieList extends Component {
 
   componentDidMount() {
     // fill me in with an HTTP Request to `localhost:5000/api/movies`
-    this.setState({ movies: [] });
+    axios.get('http://localhost:5000/api/movies')
+      .then(({ data }) => this.setState({ movies: [ ...data ] }));
   }
 
   render() {
