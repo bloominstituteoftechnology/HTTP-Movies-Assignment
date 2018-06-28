8ca62733ae7ffc6e7b9d410bbb6bbe6a1a4c7c32
Joe Stanfield <jlstan06@hotmail.com>
2018-06-28 18:44:01 -0400

    mvp finished

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -12,7 +12,16 @@ export default class MovieList extends Component {
 
   componentDidMount() {
     // fill me in with an HTTP Request to `localhost:5000/api/movies`
-    this.setState({ movies: [] });
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then(response => {
+        this.setState({ movies: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    
+    
   }
 
   render() {
