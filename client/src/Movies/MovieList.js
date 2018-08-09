fdbab853195c0f1cd60ac27036ec4c1b53ad5167
Grant Reighard <grant.reighard@gmail.com>
2018-08-09 18:17:32 -0400

    Initial commit.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -12,7 +12,11 @@ export default class MovieList extends Component {
 
   componentDidMount() {
     // fill me in with an HTTP Request to `localhost:5000/api/movies`
-    this.setState({ movies: [] });
+    axios.get('http://localhost:5000/api/movies')
+      .then(response => {
+        this.setState({ movies: response.data });
+      })
+    
   }
 
   render() {
