eab77196af76416bd7be7314bf811d73931d843a
codejoncode <jonathanjholloway@gmail.com>
2018-08-09 16:10:29 -0500

    added onChange function and added items to state in preparation to allow for adding a movie.

--- client/src/Movies/MovieCreate.js
+++ client/src/Movies/MovieCreate.js
@@ -3,7 +3,17 @@ import React from 'react';
 class MovieCreate extends React.Component {
     constructor(){
         super()
-        this.state = {}
+        this.state = { 
+            title  : '',
+            director : '', 
+            metaScore : '',
+            stars : [],
+            star : ''
+        }
+    }
+
+    onChange = event => {
+        this.setState({[event.target.name]: event.target.value})
     }
 
     render() {
@@ -12,15 +22,15 @@ class MovieCreate extends React.Component {
             <div className="movie-card formCard">
               <form>
                 <h1>Title</h1>
-                <input type="text"/>
+                <input type="text" placeholder = "title goes here" onChange ={this.onChange} name = 'title' value = {this.state.title}/>
                 <h1>Director</h1>
-                <input type="text"/>
+                <input type="text"  placeholder = "director goes here" onChange ={this.onChange} name = 'director' value = {this.state.director}/>
                 <h1>MetaScore</h1>
-                <input type="text"/>
+                <input type="text" placeholder = "metaScore goes here" onChange ={this.onChange} name = 'metaScore' value = {this.state.metaScore}/>
                 <h1>Stars</h1>
                 {/* show starts  */}
                 <h1>Add Star</h1>
-                <input type="text"/>
+                <input type="text" placeholder = "add star to list of stars" onChange ={this.onChange} name = 'star' value = {this.state.star}/>
                 <button>Add star to stars</button>
                 <h1>Submit</h1>
                 <button>Submit Movie</button>
