ae575233d54953008b70b1c51546078ffb046ece
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-08-09 16:33:39 -0400

    Form Submission handle implemented

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie'
+import Movie from './Movies/Movie';
+import MovieForm from './Movies/MovieForm';
 
 export default class App extends Component {
   constructor(){
@@ -27,6 +28,7 @@ export default class App extends Component {
         <Route path="/movies/:id" render={ (props) => {
           return(<Movie {...props} addToSavedList={this.addToSavedList}/>)
         }} />
+        <Route path="/movies/new" component={MovieForm} />
       </div>
     )
   }
--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class MovieForm extends React.Component {
   constructor(){
@@ -6,16 +7,34 @@ class MovieForm extends React.Component {
     this.state = {
       title: '',
       director: '',
-      metascore: null,
+      metascore: '',
       starone: '',
       startwo: '',
       starthree: '',
     };
   }
-  
+
+  handleInputChange = event =>{
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    const newStars = [this.state.starone, this.state.startwo, this.state.starthree];
+    const newMovie = {
+      title: this.state.title,
+      director: this.state.director,
+      metascore: parseInt(this.state.metascore, 10),
+      stars: newStars
+    };
+    axios.post('http://localhost:5000/api/movies', newMovie)
+          .then(res => this.props.history.push('/'))
+          .catch(err => console.log(err));
+  }
+
   render(){
     return(
-      <form>
+      <form onSubmit={this.handleFormSubmit}>
         <label htmlFor="title">Title:</label>
         <input
           type="text"
@@ -23,47 +42,54 @@ class MovieForm extends React.Component {
           id="title"
           value={this.state.title}
           placeholder="Enter Movie Title"
+          onChange={this.handleInputChange}
         />
-        <label htmlFor="director">Title:</label>
+        <label htmlFor="director">Director:</label>
         <input
           type="text"
           name="director"
           id="director"
           value={this.state.director}
           placeholder="Enter Movie Director"
+          onChange={this.handleInputChange}
         />
-        <label htmlFor="metascore">Title:</label>
+        <label htmlFor="metascore">Metascore:</label>
         <input
           type="number"
           name="metascore"
           id="metascore"
           value={this.state.metascore}
           placeholder="Enter Movie Metascore"
+          onChange={this.handleInputChange}
         />
-        <label htmlFor="starone">Title:</label>
+        <label htmlFor="starone">First Star:</label>
         <input
           type="text"
           name="starone"
           id="starone"
           value={this.state.starone}
           placeholder="Enter First Movie Star"
+          onChange={this.handleInputChange}
         />
-        <label htmlFor="startwo">Title:</label>
+        <label htmlFor="startwo">Second Star:</label>
         <input
           type="text"
           name="startwo"
           id="startwo"
           value={this.state.startwo}
           placeholder="Enter Second Movie Star"
+          onChange={this.handleInputChange}
         />
-        <label htmlFor="starthree">Title:</label>
+        <label htmlFor="starthree">Third Star:</label>
         <input
           type="text"
           name="starthree"
           id="starthree"
           value={this.state.starthree}
           placeholder="Enter Third Movie Star"
+          onChange={this.handleInputChange}
         />
+        <button type="submit">Submit!</button>
       </form>
     );
   }
