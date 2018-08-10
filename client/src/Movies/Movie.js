6b262724c40d3aeea817e2d0b39f6ff029d24e31
szincone <sawyerzincone@gmail.com>
2018-08-10 15:32:38 -0600

    added home button to card and styled with save button

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import MovieCard from "./MovieCard";
 export default class Movie extends React.Component {
@@ -49,6 +50,9 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+        <Link to={"/"}>
+          <div className="home-button-card">Home</div>
+        </Link>
       </div>
     );
   }
