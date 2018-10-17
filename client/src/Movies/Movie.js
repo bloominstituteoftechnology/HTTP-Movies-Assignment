d46d6e333d0ebfe7e924ee858cc39e24cfaa541d
Rahul Desai <rd825@georgetown.edu>
2018-10-17 16:17:33 -0400

    Completed main goals

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -22,6 +22,13 @@ export default class Movie extends React.Component {
   fetchMovie = id => {
     // this function needs to fire off a get request to localhost:5000/api/movies/:id
     // note that the id is dynamic.
+    axios.get(`http://localhost:5000/api/movies/${id}`)
+         .then(res => {
+           this.setState({
+             movie: res.data,
+           })
+         })
+         .catch(err => console.log(err))
   };
 
   saveMovie = () => {
