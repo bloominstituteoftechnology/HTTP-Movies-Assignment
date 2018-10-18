03b65f15972e0502b6fcd559304f822f70ba554b
Axolotl <samuelmachat@gmail.com>
2018-10-18 17:47:07 -0400

    set up movie

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,37 +1,40 @@
-import React from 'react';
-import axios from 'axios';
-import MovieCard from './MovieCard';
+import React from 'react'
+import axios from 'axios'
+import MovieCard from './MovieCard'
+
 export default class Movie extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       movie: null
-    };
+    }
   }
 
   componentDidMount() {
-    this.fetchMovie(this.props.match.params.id);
+    this.fetchMovie(this.props.match.params.id)
   }
 
   componentWillReceiveProps(newProps) {
     if (this.props.match.params.id !== newProps.match.params.id) {
-      this.fetchMovie(newProps.match.params.id);
+      this.fetchMovie(newProps.match.params.id)
     }
   }
 
   fetchMovie = id => {
-    // this function needs to fire off a get request to localhost:5000/api/movies/:id
-    // note that the id is dynamic.
-  };
+    axios
+      .get(`/api/movies/${id}`)
+      .then(res => this.setState({ movie: res.data }))
+      .catch(err => console.log(err))
+  }
 
   saveMovie = () => {
-    const addToSavedList = this.props.addToSavedList;
-    addToSavedList(this.state.movie);
-  };
+    const addToSavedList = this.props.addToSavedList
+    addToSavedList(this.state.movie)
+  }
 
   render() {
     if (!this.state.movie) {
-      return <div>Loading movie information...</div>;
+      return <div>Loading movie information...</div>
     }
 
     return (
@@ -41,6 +44,6 @@ export default class Movie extends React.Component {
           Save
         </div>
       </div>
-    );
+    )
   }
 }
