6e50c0f5d1ba2198a3a4afa9a693eb733f4b09b0
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-01-25 09:57:24 -0700

    adds axios get request to Movie List

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -12,7 +12,13 @@ export default class MovieList extends Component {
 
   componentDidMount() {
     // fill me in with an HTTP Request to `localhost:5000/api/movies`
-    this.setState({ movies: [] });
+    axios
+      .get('http://localhost:5000/api/movies')
+      .then(response => {
+        this.setState({ movies: response.data })
+      })
+      .catch(error => console.log(error))
+    ;
   }
 
   render() {
