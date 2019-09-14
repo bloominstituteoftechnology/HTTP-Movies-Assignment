42e1b9ee6e7d9477225df48d2487748d857cf59a
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-14 13:21:21 -0700

    update movie works

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,60 +1,118 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const UpdateMovie = (props) => {
+const UpdateMovie = props => {
+  const [movie, setMovie] = useState({
+    id: "",
+    title: "",
+    director: "",
+    metascore: "",
+    stars: []
+  });
 
-    const [movie, setMovie] = useState({
-        id: "",
-        title: "",
-        director: "",
-        metascore: "",
-        stars: ""
+  useEffect(() => {
+    //   console.log(props.history);
+    const id = props.match.params.id;
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then(res => {
+        setMovie({
+          id: id,
+          title: res.data.title,
+          director: res.data.director,
+          metascore: res.data.metascore,
+          stars: res.data.stars
+        });
+      })
+
+      .catch(err => console.log(err));
+  }, []);
+
+  const handleChange = e => {
+
+    setMovie({
+      ...movie,
+      [e.target.name]: e.target.value
     });
 
-    useEffect(()=>{
-        const id = props.match.params.id;
-        axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(res=>{
-            console.log(res)
-            setMovie({
-                id: id,
-                title: res.data.title,
-                director:  res.data.director,
-                metascore: res.data.metascore,
-                stars:  res.data.stars
+    // console.log(movie);
+  };
+
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(movie.title != "" && movie.director != "" && movie.stars != "" && movie.metascore != ""){
+
+            console.log(movie)
+          console.log("submit handler");
+        if(typeof(movie.stars)=="string"){
+
+            const stars = movie.stars.split(',');
+
+            const newMovie = {
+                ...movie,
+                stars: stars
+            }
+
+            axios
+            .put(`http://localhost:5000/api/movies/${props.match.params.id}`, newMovie)
+            .then(res=>{
+                console.log(res);
+                props.history.push('/')
             })
+            .catch(err=>{
+                console.log(err)
+            });
 
-        })
             
-            
-        
-        
-        .catch(err=>console.log(err));
-    },[])
+        } else {
+            console.log('running else');
+            axios
+            .put(`http://localhost:5000/api/movies/${props.match.params.id}`, movie)
+            .then(res=>{
+                console.log(res);
+                props.history.push('/')
+            })
+            .catch(err=>{
+                console.log(err)
+            });
+        }
+    
 
-    const handleChange = e =>{
-        setMovie({
-            ...movie,
-            [e.target.name]: e.target.value
-        })
     }
+    
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        onChange={handleChange}
+        value={movie.title}
+        name="title"
+        placeholder="Title"
+      />
+      <input
+        onChange={handleChange}
+        value={movie.director}
+        name="director"
+        placeholder="Director"
+      />
+      <input
+        onChange={handleChange}
+        value={movie.metascore}
+        name="metascore"
+        placeholder="Metascore"
+      />
+      <textarea onChange={handleChange}
+        value={movie.stars}
+        name="stars"
+        placeholder="Stars - Seperate by commas"/>
+      {/* <input
+        
+      /> */}
+      {/* Separate stars with commas the split into array? */}
+      <button>Update Movie</button>
+    </form>
+  );
+};
 
-    const handleSubmit = e =>{
-        e.preventDefault();
-        console.log('submit handler');
-        console.log(props);
-    }
-    return ( 
-        <form onSubmit={handleSubmit}>
-            <input onChange={handleChange} value={movie.title} name="title" placeholder="Title" />
-            <input onChange={handleChange} value={movie.director} name="director" placeholder="Director" />
-            <input onChange={handleChange} value={movie.metascore} name="metascore" placeholder="Metascore" />
-            <input onChange={handleChange} value={movie.stars} name="stars" placeholder="Stars - Seperate by commas" /> 
-            {/* Separate stars with commas the split into array? */}
-            <button>Update Movie</button>
-        </form>
-     );
-}
- 
 export default UpdateMovie;
