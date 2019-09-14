5a1497d70ae56641aebd92b05b4196ce52180428
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-14 12:53:29 -0700

    edit form updating with movie content

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,15 +1,44 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 const UpdateMovie = (props) => {
+
+    const [movie, setMovie] = useState({
+        id: "",
+        title: "",
+        director: "",
+        metascore: "",
+        stars: ""
+    });
 
     useEffect(()=>{
         const id = props.match.params.id;
         axios
         .get(`http://localhost:5000/api/movies/${id}`)
-        .then(res=>console.log(res))
+        .then(res=>{
+            console.log(res)
+            setMovie({
+                id: id,
+                title: res.data.title,
+                director:  res.data.director,
+                metascore: res.data.metascore,
+                stars:  res.data.stars
+            })
+
+        })
+            
+            
+        
+        
         .catch(err=>console.log(err));
     },[])
+
+    const handleChange = e =>{
+        setMovie({
+            ...movie,
+            [e.target.name]: e.target.value
+        })
+    }
 
     const handleSubmit = e =>{
         e.preventDefault();
@@ -18,10 +47,10 @@ const UpdateMovie = (props) => {
     }
     return ( 
         <form onSubmit={handleSubmit}>
-            <input placeholder="Title" />
-            <input placeholder="Director" />
-            <input placeholder="Metascore" />
-            <input placeholder="Stars" /> 
+            <input onChange={handleChange} value={movie.title} name="title" placeholder="Title" />
+            <input onChange={handleChange} value={movie.director} name="director" placeholder="Director" />
+            <input onChange={handleChange} value={movie.metascore} name="metascore" placeholder="Metascore" />
+            <input onChange={handleChange} value={movie.stars} name="stars" placeholder="Stars - Seperate by commas" /> 
             {/* Separate stars with commas the split into array? */}
             <button>Update Movie</button>
         </form>
