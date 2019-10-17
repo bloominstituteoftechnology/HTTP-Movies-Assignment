abfd6f110a59842407444403c33e4e1c073adf43
Pere <pere.solaclaver@gmail.com>
2019-10-17 13:28:24 +0100

    Create update button linking to URL to edit the movie

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 import MovieCard from "./MovieCard";
+import { Link } from 'react-router-dom';
+
 export default class Movie extends React.Component {
   constructor(props) {
     super(props);
@@ -42,6 +44,7 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+        <Link to={`/update-movie/${this.state.movie.id}`}>Update</Link>
       </div>
     );
   }
---
+++ client/src/Movies/UpdateMovieForm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function UpdateMovieForm(props) {
+    debugger
+    return (
+        <div>hello from the form component</div>
+    )
+}
