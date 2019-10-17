46213626ddf5c1a9760c45fa2b73feffb200219b
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-17 12:55:08 +0100

    :lipstick: Further styling

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import "react-bulma-components/dist/react-bulma-components.min.css";
+import UpdateMovie from "./Movies/UpdateMovie";
 
 
 const App = () => {
@@ -26,7 +27,7 @@ const App = () => {
       <Route
         path="/update-movie/:id"
         render={props => {
-          return <Movie {...props} />;
+          return <UpdateMovie {...props} />;
         }}
       />
     </>
--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,6 +1,40 @@
 import React from 'react';
+import { Formik, Form, Field } from "formik";
+import styled from "styled-components";
+
+import { Button, Card, Heading, Content } from "react-bulma-components";
+
 
 export default function UpdateMovie(props) {
+    return <Formik
+        initialValues = {{}}
+        onSubmit = {() => {}}
+        render = {props => {
+            return <StyledForm>
+                <Field name='title' type='text' placeholder='Title' />
+                <Field name='metascore' type='number' placeholder='Metascore' />
+                <Field name='director' type='text' placeholder='Director' />
 
+                <Button color='primary' type='submit' >Submit</Button>
+            </StyledForm>
+        }}
+    />
 }
 
+
+const StyledForm = styled(Form)`
+    display: flex;
+    margin: 0 auto;
+    width: 30rem;
+    flex-direction: column;
+
+    input {
+        width: 23rem;
+        margin: 0.6rem auto;
+    }
+
+    button {
+        width: 8rem;
+        margin: 1rem auto;
+    }
+`
