12e19a99d8a162fe489238e879531875fdde62d7
vy3191 <venkyafhs@gmail.com>
2019-11-10 11:31:38 -0500

    Completed stretch goal - adding a movie

--- client/src/Movies/AddMovie.js
+++ client/src/Movies/AddMovie.js
@@ -2,19 +2,26 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 function AddMovie(props) {
-  const defaultMovie = {id: "", tittle:"", director:"", metascore:"",stars:[]}
+  const defaultStars = ["", "", ""]
+  const defaultMovie = {id: "", title:"", director:"", metascore:"",stars:defaultStars}
   const [newMovie, setNewMovie] = useState(defaultMovie);
   const [stars, setStars] = useState({});
   const handleChange = (event) => {
      setNewMovie({...newMovie, [event.target.name]: event.target.value});
   }
 
+  const handleStars = (event) => {
+     setStars({...stars, [event.target.name]: event.target.value});
+     setNewMovie({...newMovie, stars:Object.values(stars)})
+  }
   const handleSubmit = (event) => {
      event.preventDefault();
+     console.log(newMovie);
      axios.post(`http://localhost:5000/api/movies/`, newMovie)
           .then (res => {
              console.log(res);
              props.history.push("/");
+             setNewMovie(defaultMovie);
           })
           .catch( err => {
              console.log(err);
@@ -49,6 +56,30 @@ function AddMovie(props) {
               value={newMovie.metascore}
               require="required"
        /></label><br />
+       <div>
+         <p>Movie Stars:</p>
+         <input type="text"
+              onChange={(event) =>handleStars(event)}
+              name="index-0"
+              placeholder="star-0"
+              value={stars[0]}
+              require="required"
+          /><br />
+          <input type="text"
+              onChange={(event) =>handleStars(event)}
+              name="index-1"
+              placeholder="star-1"
+              value={stars[1]}
+              require="required"
+          /><br />
+          <input type="text"
+              onChange={(event) =>handleStars(event)}
+              name="index-2"
+              placeholder="star-2"
+              value={stars[2]}
+              require="required"
+          /><br />
+       </div>
        <div>
          <button type="submit">Add Movie</button>
        </div>
