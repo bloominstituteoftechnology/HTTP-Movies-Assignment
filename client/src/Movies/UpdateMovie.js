798b5b33efc42bc18a640f0609d6f78ab0ea6df4
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-11-14 15:53:18 -0800

    added useEffect

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -13,37 +13,58 @@ const initialMovie = {
 const UpdateMovie = (props) => {
     const [movie, setMovie] = useState(initialMovie)
 
+    useEffect(() => {
+        const movieToUpdate = movies.find(movie => `${movie.id}` === id);
+    },[])
+
+    const handleChange = e => {
+        e.persist();
+        setMovie({...movie, [e.target.name]: [e.target.value]})
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios
+          .put('')
+          .then(res => { 
+              console.log(res)
+              props.history.push(`/movies/${movie.id}`)
+              setMovie(initialMovie);
+          })
+          .catch(err => console.log(err))
+    }
+
     return(
         <div>
             <h2>Update Movie</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     type='text'
                     name='title'
                     placeholder='title'
-                    value=''
-                    onChange='' 
+                    value={movie.title}
+                    onChange={handleChange} 
                 />
                 <input
                     type='text'
                     name='director'
                     placeholder='director'
-                    value=''
-                    onChange=''  
+                    value={movie.director}
+                    onChange={handleChange}  
                 />
                 <input
                     type='text'
                     name='metascore'
                     placeholder='metascore'
-                    value=''
-                    onChange=''  
+                    value={movie.metascore}
+                    onChange={handleChange} 
                 />
                 <input
                     type='text'
                     name='stars'
                     placeholder='stars'
-                    value=''
-                    onChange=''  
+                    value={movie.stars}
+                    onChange={handleChange} 
                 />
                 <button>Update</button>
             </form>
