275b8c81b0ddf7632fcd8b7e01d0868710aaff8a
CLYPR Technologies <cbanks@clypr.co>
2020-01-22 20:53:00 -0500

    mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 import MovieCard from "./MovieCard";
+import { Link } from 'react-router-dom';
+
 export default class Movie extends React.Component {
   constructor(props) {
     super(props);
@@ -31,6 +33,18 @@ export default class Movie extends React.Component {
     addToSavedList(this.state.movie);
   };
 
+  handleDelete = () => {
+    axios
+    .delete('http://localhost:5000/api/movies/${this.props.match.params.id}')
+    .then(res => {
+      console.log(res);
+      this.props.history.push('/');
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  };
+
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
@@ -42,6 +56,8 @@ export default class Movie extends React.Component {
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+        <Link to={'/update-movie/${this.props.match.params.id}'}>Update Movie</Link>
+        <button onClick={this.handleDelete}>Delete Movie</button>
       </div>
     );
   }
