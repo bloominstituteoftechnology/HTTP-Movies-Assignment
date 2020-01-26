e089556afdeefccf725aa8981ebd3f06dd39a4c8
igor <embiggenerd@gmail.com>
2020-01-26 17:34:21 -0500

    styling less poopoo

--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
 import axios from 'axios'
 
-export const MovieForm = ({updateMovie}) => {
+export const MovieForm = ({ updateMovie }) => {
     const [movie, setMovie] = useState({})
     const history = useHistory()
     const { id } = useParams()
@@ -33,18 +33,28 @@ export const MovieForm = ({updateMovie}) => {
             const { data } = await axios.put(`http://localhost:5000/api/movies/${id}`, { ...movie, stars: starsArray })
             updateMovie(data)// setmovies to new array
             history.push("/")// redirect to 
-        } catch(e) {
+        } catch (e) {
             console.log('putError', e.response)
         }
     }
 
     return (
-        <div><form onSubmit={handleSubmitUpdate}>
-            <input value={movie.title || ""} name="title" onChange={handleOnChange} />
-            <input value={movie.director || ""} name="director" onChange={handleOnChange} />
-            <input value={movie.metascore || ""} name="metascore" onChange={handleOnChange} />
-            <input value={movie.stars || ""} name="stars" onChange={handleOnChange} />
-            <input type="submit" value="update"/>
-        </form></div>
+        <div>
+            <form className="update-form" onSubmit={handleSubmitUpdate}>
+                <label htmlFor="title">Title:</label>
+                <input id="title" value={movie.title || ""} name="title" onChange={handleOnChange} />
+
+                <label htmlFor="director">Director:</label>
+                <input id="director" value={movie.director || ""} name="director" onChange={handleOnChange} />
+
+                <label htmlFor="metascore">Metascore:</label>
+                <input id="metascore" value={movie.metascore || ""} name="metascore" onChange={handleOnChange} />
+
+                <label htmlFor="stars">Stars:</label>
+                <textarea id="stars" value={movie.stars || ""} name="stars" onChange={handleOnChange} />
+
+                <input type="submit" value="update" />
+            </form>
+        </div>
     )
 }
