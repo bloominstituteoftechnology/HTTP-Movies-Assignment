486e5719347e89ac7143638ca010d6a2a1651f44
igor <embiggenerd@gmail.com>
2020-01-29 12:56:42 -0500

    mvp plus stretch finished

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -13,6 +13,7 @@ export default class MovieList extends Component {
   }
 
   componentDidMount() {
+    console.log('moveList did mount')
     axios
       .get("http://localhost:5000/api/movies")
       .then(res => this.setState({ movies: res.data }))
@@ -22,6 +23,7 @@ export default class MovieList extends Component {
   render() {
     return (
       <div className="movie-list">
+        <button onClick={() => this.props.history.push('/')}>Add Movie</button>
         {this.state.movies.map(movie => (
           <MovieDetails key={movie.id} movie={movie} />
         ))}
