4711238969018bb7089d0ff29b512053adca56df
Jonathan Hanson <jhanson8@uoregon.edu>
2020-01-29 13:05:44 -0800

    project set up

--- server.js
+++ server.js
@@ -107,6 +107,6 @@ app.get("/", function(req, res) {
   res.send("App is working 👍");
 });
 
-app.listen(5000, () => {
-  console.log("Server listening on port 5000");
+app.listen(5001, () => {
+  console.log("Server listening on port 5001");
 });
