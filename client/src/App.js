af3d4ff596940e9ea53fb8c592ac8738b0f43f99
wesley.moody <wgm3190@gmail.com>
2020-01-29 15:37:46 -0600

    add movies component and button created

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import UpdateMovie from "./Movies/UpdateMovie";
+import AddMovie from "./Movies/AddMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -15,16 +16,16 @@ const App = () => {
   return (
     <>
       <SavedList list={savedList} />
-      
-      <Route exact path="/" component={MovieList} />
+
+      <Route exact path='/' component={MovieList} />
       <Route
-        path="/movies/:id"
+        path='/movies/:id'
         render={props => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
-
         }}
       />
-            <Route path="/update-movie/:id" />
+      <Route path='/update-movie/:id' />
+      <Route path='/add-movie' component={AddMovie} />
     </>
   );
 };
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { NavLink, Link } from "react-router-dom";
+
 export default class SavedList extends Component {
   constructor(props) {
     super(props);
@@ -7,22 +8,24 @@ export default class SavedList extends Component {
 
   render() {
     return (
-      <div className="saved-list">
+      <div className='saved-list'>
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => {
           return (
             <NavLink
               to={`/movies/${movie.id}`}
               key={movie.id}
-              activeClassName="saved-active"
-            >
-              <span className="saved-movie">{movie.title}</span>
+              activeClassName='saved-active'>
+              <span className='saved-movie'>{movie.title}</span>
             </NavLink>
           );
         })}
-        <div className="home-button">
-          <Link to="/">Home</Link>
+        <div className='home-button'>
+          <Link to='/'>Home</Link>
         </div>
+        <Link to={`/add-movie`}>
+          <button>Add Movie</button>
+        </Link>
       </div>
     );
   }
