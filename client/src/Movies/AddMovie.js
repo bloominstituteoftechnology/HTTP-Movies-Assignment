50cde1758d9ea03654f66cef82d73dada05b1061
wesley.moody <wgm3190@gmail.com>
2020-01-30 18:55:45 -0600

    updated some styling

--- client/src/Movies/AddMovie.js
+++ client/src/Movies/AddMovie.js
@@ -18,42 +18,52 @@ const AddMovie = props => {
   };
 
   const handleSubmit = e => {
-    axios.post(`http://localhost:5000/api/movies/`,add)
-    .then(res => {props.history.push("/")})
-    .catch(err => console.log(err))
-    e.preventDefault()
-}
+    axios
+      .post(`http://localhost:5000/api/movies/`, add)
+      .then(res => {
+        props.history.push("/");
+      })
+      .catch(err => console.log(err));
+    e.preventDefault();
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <h1>{add.title}</h1>
-      <p>{add.director}</p>
-      <p>{add.metascore}</p>
-      <p>{add.stars}</p>
-      <input
-        name='title'
-        placeholder='Title'
-        value={add.title}
-        onChange={handleChange}
-      />
-      <input
-        name='director'
-        placeholder='Director'
-        value={add.director}
-        onChange={handleChange}
-      />
-      <input
-        name='metascore'
-        placeholder='Metascore'
-        value={add.metascore}
-        onChange={handleChange}
-      />
-      <input
-        name='stars'
-        placeholder='Stars'
-        value={add.stars}
-        onChange={handleChange}
-      />
+      <h1 className='title'>Add Movie Info</h1>
+      <div className='addMovie'>
+        <input
+          name='title'
+          placeholder='Title'
+          value={add.title}
+          onChange={handleChange}
+        />
+        <input
+          name='director'
+          placeholder='Director'
+          value={add.director}
+          onChange={handleChange}
+        />
+        <input
+          name='metascore'
+          placeholder='Metascore'
+          value={add.metascore}
+          onChange={handleChange}
+        />
+        <input
+          name='stars'
+          placeholder='Stars'
+          value={add.stars}
+          onChange={handleChange}
+        />
+        <button>Submit</button>
+      </div>
+      <h1 className='title'>Card Preview</h1>
+      <div className='cardPreview'>
+        <h1>Title:{add.title}</h1>
+        <p>Director:{add.director}</p>
+        <p>Metascore:{add.metascore}</p>
+        <p>Actors:{add.stars}</p>
+      </div>
     </form>
   );
 };
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -4,22 +4,18 @@ const MovieCard = props => {
   const { title, director, metascore, stars } = props.movie;
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
+      <p>{title}</p>
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
+      <div className="movie-metascore">Metascore:<strong>{metascore}</strong></div>
+      <p>Actors</p>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
+      
+      <div>{stars}</div>          
     </div>
   );
 };
+  
 
 export default MovieCard;
