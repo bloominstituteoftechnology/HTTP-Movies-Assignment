e5df85d48a9c2171e10bbba7ebb78523d35ba460
taty2010 <shydalys123@gmail.com>
2020-02-19 23:37:50 -0600

    added axios and useffect for movies to propdrill down to the UpdateForm component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import UpdateForm from './Movies/UpdateForm';
+import axios from 'axios'
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [movie, setMovie] = useState([]);
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
 
+  useEffect(()=>{
+    axios
+    .get("http://localhost:5000/api/movies")
+    .then(res => setMovie( res.data ))
+    .catch(err => console.log(err.response));
+  }, [])
   return (
     <>
       <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
+      <Route exact path="/" render={props => {
+        return <MovieList {...props} addToSavedList={addToSavedList}/>
+      }} />
       <Route
         path="/movies/:id"
         render={props => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
         }}
       />
+      <Route path='/update-movie/:id'
+    render={props => {
+      return <UpdateForm {...props} movie={movie} addToSavedList={setMovie}/>
+    }}
+    />
     </>
   );
 };
