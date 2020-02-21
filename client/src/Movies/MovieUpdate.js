f271d5e66a6da93ec121f8c9ab1d0a48bfec6389
Veto Ramirez <vetorobert3@gmail.com>
2020-02-20 19:58:31 -0800

    Working on MovieUpdate component.

--- client/src/Movies/MovieUpdate.js
+++ client/src/Movies/MovieUpdate.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const MovieUpdate = () => {
+
+  updatedMovie = (id, updateMovie) => {
+    axios
+    .put(`http://localhost:5000/ap/movie/${id}`, updateMovie)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  }
+
   return null
 }
 
