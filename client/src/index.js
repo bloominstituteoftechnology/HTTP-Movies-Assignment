f623ac1453c98396f3328f168a28be6a04cfa8f6
James Hargis <okcomputer@go-fuck-yourself.local>
2020-03-17 15:30:17 -0500

    Init Commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import './index.css';
 
 ReactDOM.render(
   <Router>
