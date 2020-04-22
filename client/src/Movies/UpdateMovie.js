d143a009d41ea30664f169ac2f26e971e2d77833
Michael Perez <mperez6473@gmail.com>
2020-04-22 15:13:37 -0500

    There is now a handleSubmit for the form and the form has been created.

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import axios from 'axios';
+import e from 'express';
 
 const someMovie = {
     id: '',
@@ -36,9 +37,53 @@ const UpdateMovie = (props) => {
             [event.target.name]: value,
         });
     };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axios
+            .put(``, movie)
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
     return (
         <div>
             <h1>Welcome to the Update Movie Form</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="title"
+                    onChange={handleChanges}
+                    placeholder="title"
+                    value={movie.title}
+                />
+                <input
+                    type="text"
+                    name="director"
+                    onChange={handleChanges}
+                    placeholder="director"
+                    value={movie.director}
+                />
+                <input
+                    type="text"
+                    name="metascore"
+                    onChange={handleChanges}
+                    placeholder="title"
+                    value={movie.metascore}
+                />
+                <input
+                    type="string"
+                    name="stars"
+                    onChange={handleChanges}
+                    placeholder="stars"
+                    value={movie.stars}
+                />
+                <button>Update This Movie</button>
+            </form>
         </div>
     );
 };
