7761c9b0f302aee5d2cb5d18dd075db0c9f69928
Michael Perez <mperez6473@gmail.com>
2020-04-22 16:33:33 -0500

    Have the edit page appearing now. There is a button on each movie card with a edit on it. Takes you to a form.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,40 +1,44 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import MovieCard from "./MovieCard";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams, Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
+import UpdateMovie from './UpdateMovie';
 
 function Movie({ addToSavedList }) {
-  const [movie, setMovie] = useState(null);
-  const params = useParams();
+    const [movie, setMovie] = useState(null);
+    const params = useParams();
 
-  const fetchMovie = (id) => {
-    axios
-      .get(`http://localhost:5000/api/movies/${id}`)
-      .then((res) => setMovie(res.data))
-      .catch((err) => console.log(err.response));
-  };
+    const fetchMovie = (id) => {
+        axios
+            .get(`http://localhost:5000/api/movies/${id}`)
+            .then((res) => setMovie(res.data))
+            .catch((err) => console.log(err.response));
+    };
 
-  const saveMovie = () => {
-    addToSavedList(movie);
-  };
+    const saveMovie = () => {
+        addToSavedList(movie);
+    };
 
-  useEffect(() => {
-    fetchMovie(params.id);
-  }, [params.id]);
+    useEffect(() => {
+        fetchMovie(params.id);
+    }, [params.id]);
 
-  if (!movie) {
-    return <div>Loading movie information...</div>;
-  }
+    if (!movie) {
+        return <div>Loading movie information...</div>;
+    }
 
-  return (
-    <div className="save-wrapper">
-      <MovieCard movie={movie} />
+    return (
+        <div className="save-wrapper">
+            <MovieCard movie={movie} />
 
-      <div className="save-button" onClick={saveMovie}>
-        Save
-      </div>
-    </div>
-  );
+            <div className="save-button" onClick={saveMovie}>
+                Save
+            </div>
+            <Link to={`/update-movie/${movie.id}`}>
+                <button>Edit</button>
+            </Link>
+        </div>
+    );
 }
 
 export default Movie;
