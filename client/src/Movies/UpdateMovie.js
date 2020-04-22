e688df9eb3b1093fa9b167c71b0121e073516d24
Michael Perez <mperez6473@gmail.com>
2020-04-22 15:00:34 -0500

    Getting the UpdateMovie file set up. So far it has an empty movie setup, UpdateForm funciton that pulls from axios, handleChanges function, and setMovie from state.

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+const someMovie = {
+    id: '',
+    title: '',
+    director: '',
+    metascore: '',
+    stars: [],
+};
+
+const UpdateForm = (props) => {
+    const { push } = useHistory();
+    const [movie, setMovie] = useState(someMovie)
+    const { id } = useParams()
+    useEffect(() => {
+        axios.get(``)
+            .then(res => {
+                console.log(res -)
+            })
+            .catch(err =>{console.log(err)})
+    }, [id])
+};
+
+const handleChanges = event => {
+    event.persist()
+    let value = event.target.value;
+    if (event.target.name === 'title') {
+        value = parseInt(value, 10)
+    }
+
+    setMovie({
+        ...movie,
+        [event.target.name]: value
+    })
+}
+
+
 
 const UpdateMovie = () => {
     return (
         <div>
-            <h1>test</h1>
+            <h1>Welcome to the Update Movie Form</h1>
         </div>
     );
 };
