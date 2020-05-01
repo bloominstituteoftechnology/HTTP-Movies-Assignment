16de5a6149c4d2169ec06b5bbe5c7095d7354aa1
Veto Ramirez <vetorobert3@gmail.com>
2020-04-30 19:52:56 -0700

    Working on updating data.

--- client/src/Movies/MovieUpdate.js
+++ client/src/Movies/MovieUpdate.js
@@ -23,52 +23,57 @@ const MovieUpdate = (props) => {
   const handleSubmit = e => {
     e.preventDefault();
     axios 
-      .put(``, movie)
+      .put(`http://localhost:5000/api/movies/${movie.id}`, movie)
       .then(() => props.history.push('/'))
       .catch(err => console.log(err))
   }
 
   useEffect(() => {
     axios 
-      .get(``)
+      .get(`http://localhost:5000/api/movies`)
       .then(res => setMovie(res.data))
       .catch(err => console.log(err))
-  })
+  }, [])
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="title"
-          onChange={changeHandler}
-          placeholder="Title"
-          value={movie.name}
-        />
-
-        <input
-          type="text"
-          name="director"
-          onChange={changeHandler}
-          placeholder="Director"
-          value={movie.director}
-        />
-
-        <input
-          type="number"
-          name="metascore"
-          onChange={changeHandler}
-          placeholder="Metascore"
-          value={movie.metascore}
-        />
-
-        <input
-          type="text"
-          name="start"
-          onChange={changeHandler}
-          placeholder="Stars"
-          value={movie.stars}
-        />
+        <div>
+          <input
+            type="text"
+            name="title"
+            onChange={changeHandler}
+            placeholder="Title"
+            value={movie.name}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="director"
+            onChange={changeHandler}
+            placeholder="Director"
+            value={movie.director}
+          />
+        </div>
+        <div>
+          <input
+            type="number"
+            name="metascore"
+            onChange={changeHandler}
+            placeholder="Metascore"
+            value={movie.metascore}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="start"
+            onChange={changeHandler}
+            placeholder="Stars"
+            value={movie.stars}
+          />
+        </div>
 
         <button type='submit'>Update</button>
       </form>
