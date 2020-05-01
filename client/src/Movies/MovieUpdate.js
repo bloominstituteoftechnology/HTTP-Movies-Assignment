773665476fb9954f4f7f8b7a2e9134b36ea66272
Veto Ramirez <vetorobert3@gmail.com>
2020-04-30 19:39:35 -0700

    Added form to Movie update component and set up onSubmit and changeHandler.

--- client/src/Movies/MovieUpdate.js
+++ client/src/Movies/MovieUpdate.js
@@ -1,22 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Movie from './Movie';
+
+const initialMovie = {
+  id: '',
+  title: '',
+  director: '',
+  metascore: '',
+  stars: []
+};
 
 const MovieUpdate = (props) => {
-  const [update, setUpdate] = useState({
-    id: '',
-    title: '',
-    director: '',
-    metascore: '',
-    stars: []
-  });
+  const [movie, setMovie] = useState(initialMovie);
+
+  const changeHandler = e => {
+    setMovie({
+      ...movie, 
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios 
+      .put(``, movie)
+      .then(() => props.history.push('/'))
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    axios 
+      .get(``)
+      .then(res => setMovie(res.data))
+      .catch(err => console.log(err))
+  })
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          onChange={changeHandler}
+          placeholder="Title"
+          value={movie.name}
+        />
 
+        <input
+          type="text"
+          name="director"
+          onChange={changeHandler}
+          placeholder="Director"
+          value={movie.director}
+        />
+
+        <input
+          type="number"
+          name="metascore"
+          onChange={changeHandler}
+          placeholder="Metascore"
+          value={movie.metascore}
+        />
+
+        <input
+          type="text"
+          name="start"
+          onChange={changeHandler}
+          placeholder="Stars"
+          value={movie.stars}
+        />
+
+        <button type='submit'>Update</button>
       </form>
-      
-      <button>Update</button>
     </div>
   )
 }
