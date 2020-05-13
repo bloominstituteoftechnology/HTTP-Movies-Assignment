e7befac04120a4a0530f5c7dbb42bcfe273ba7bd
Ariel_Rodriguez <32606708+Ariels713@users.noreply.github.com>
2020-05-13 17:50:26 -0400

    updated form

--- README.md
+++ README.md
@@ -10,9 +10,9 @@
 
 ### Part 1 - Updating A Movie:
 
-- Add a route at the path `/update-movie/:id`
-- Create a component with a form to update the chosen movie
-- Add a button in the movie component that routes you to your new route with the movies's id as the URL param
+✅- Add a route at the path `/update-movie/:id`
+✅- Create a component with a form to update the chosen movie
+✅- Add a button in the movie component that routes you to your new route with the movies's id as the URL param
 - The form should make a PUT request to the server when submitted
 - When the call comes back successfully, reset your form state and route the user to `/movies` where they will see the updated movie in the list
 
--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,89 +1,87 @@
-import React, { useState } from 'react'
-import { Button, Form } from 'semantic-ui-react'
-import Axios from 'axios'
+import React, { useState } from "react";
+import { Button, Form, Header } from "semantic-ui-react";
+import Axios from "axios";
 import { useParams, useHistory } from "react-router-dom";
 
-const UpdateMovie = (props) => {
-    const { id } = useParams();
-    console.log("id", id)
-    const { push } = useHistory()
-    const [formData, setFormData] = useState({
-        id: `${id}`,
-        title: "",
-        director: "",
-        metascore: "",
-        // actors: []
-    })
+const UpdateMovie = () => {
+  const { id } = useParams();
+  console.log("id", id);
+  const { push } = useHistory();
+  const [formData, setFormData] = useState({
+    id: `${id}`,
+    title: "",
+    director: "",
+    metascore: "",
+    stars: []
+  });
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    Axios.put(`http://localhost:5000/api/movies/${id}`, formData)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    push("./");
+  };
 
-    const submitHandler = (event) => {
-        event.preventDefault()
-        Axios
-          .put(`http://localhost:5000/api/movies/${id}`, formData)
-          .then(res => console.log(res))
-          .catch(err => console.log(err))
-          push('./')
-          
-          
-    }
+  const changeHandler = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
 
-    const changeHandler = (event) => {
-        const { name, value } = event.target;
-        setFormData((prevState) => {
-            return {
-                ...prevState,
-                [name] : value
-            }
-        })
-    }
+  return (
+    <Form
+      onSubmit={submitHandler}
+      style={{ maxWidth: "400px", margin: "100px auto" }}
+    >
+      <Header as="h1">Update Movie Details</Header>
+      <Form.Field>
+        <label>Title</label>
+        <input
+          placeholder="Title"
+          type="text"
+          name="title"
+          onChange={changeHandler}
+          value={formData.title}
+        />
+      </Form.Field>
+      <Form.Field>
+        <label>Director</label>
+        <input
+          placeholder="Title"
+          type="text"
+          name="director"
+          onChange={changeHandler}
+          value={formData.director}
+        />
+      </Form.Field>
+      <Form.Field>
+        <label>Metascore:</label>
+        <input
+          placeholder="Title"
+          type="text"
+          name="metascore"
+          onChange={changeHandler}
+          value={formData.meta}
+        />
+      </Form.Field>
+      <Form.Field>
+        <label>Staring:</label>
+        <input
+          placeholder="Title"
+          type="text"
+          name="actors"
+          onChange={changeHandler}
+          value={formData.actors}
+        />
+      </Form.Field>
+      <Button type="submit">Submit</Button>
+    </Form>
+  );
+};
 
-    return (
-        <Form onSubmit={submitHandler} style={{maxWidth:"400px", margin:"0 auto"}}>
-            <Form.Field>
-                <label>Title</label>
-                <input
-                    placeholder='Title'
-                    type="text"
-                    name="title"
-                    onChange={changeHandler}
-                    value={formData.title}
-                />
-            </Form.Field>
-            <Form.Field>
-                <label>Director</label>
-                <input
-                    placeholder='Title'
-                    type="text"
-                    name="director"
-                    onChange={changeHandler}
-                    value={formData.director}
-                />
-            </Form.Field>
-            <Form.Field>
-                <label>Metascore:</label>
-                <input
-                    placeholder='Title'
-                    type="text"
-                    name="metascore"
-                    onChange={changeHandler}
-                    value={formData.meta}
-                />
-            </Form.Field>
-            <Form.Field>
-                <label>Staring:</label>
-                <input
-                    placeholder='Title'
-                    type="text"
-                    name="actors"
-                    onChange={changeHandler}
-                    value={formData.actors}
-                />
-            </Form.Field>
-            <Button type='submit'>Submit</Button>
-        </Form>
-    )
-}
-
-export default UpdateMovie
-
-
+export default UpdateMovie;
