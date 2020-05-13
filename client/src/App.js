68d99eb5421742c0a8a7ef7b188c678a909cca41
jimmoua1 <tsheejmaus@yahoo.com>
2020-05-13 13:22:01 -0700

    remove movielist from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,22 +7,10 @@ import axios from 'axios';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
-  const [movieList, setMovieList] = useState([]);
-
-  const getMovieList = () => {
-    axios
-      .get("http://localhost:5000/api/movies")
-      .then(res => setMovieList(res.data))
-      .catch(err => console.log(err.response));
-  };
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
-
-  useEffect(() => {
-    getMovieList();
-  }, []);
 
   return (
     <>
