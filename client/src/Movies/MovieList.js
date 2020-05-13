57209a6960070c22bb49a5340f4a57c758a2875d
jimmoua1 <tsheejmaus@yahoo.com>
2020-05-13 13:49:29 -0700

    added MovieList,js

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import MovieCard from "./MovieCard";
+export default class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    };
+  }
 
-function MovieList({ movies }) {
-  return (
-    <div className="movie-list">
-      {
-        movies.map(movie => (
-          <Link key={movie.id} to={`/movies/${movie.id}`}>
-            <MovieCard movie={movie} />
-          </Link>
-        ))
-      }
-    </div>
-  );
+  componentDidMount() {
+    console.log("Mounted");
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then(res => {
+        console.log(res);
+        this.setState({ movies: res.data });
+      })
+      .catch(err => console.log(err.response));
+  }
+
+  render() {
+    console.log("render Movie List");
+    return (
+      <div className='movie-list'>
+        {this.state.movies.map(movie => (
+          <MovieDetails key={movie.id} movie={movie} />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default MovieList;
+function MovieDetails({ movie }) {
+  return (
+    <Link to={`/movies/${movie.id}`}>
+      <MovieCard movie={movie} />
+    </Link>
+  );
+}
