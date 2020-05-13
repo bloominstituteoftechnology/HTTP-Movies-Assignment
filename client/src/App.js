965c8e12ee40304348dc6689dbddec94119e4882
Ariel Rodriguez <Ariels713@gmail.com>
2020-05-13 17:35:43 +0000

    creating form to update movies

--- client/src/App.js
+++ client/src/App.js
@@ -35,6 +35,9 @@ const App = () => {
       <Route path="/movies/:id">
         <Movie addToSavedList={addToSavedList} />
       </Route>
+      <Route path="/update-movie/:id">
+        
+      </Route>
     </>
   );
 };
---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateMovie (props) => {
+    return (
+        
+    )
+}
+
+export default UpdateMovie
