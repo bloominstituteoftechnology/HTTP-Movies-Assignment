6c114ef79d6d8ee9d32b8a6289972717466a2c9f
Your Name <Reginaldbmiller@gmail.com>
2020-05-14 19:59:19 -0400

    mvp but dependency issues remain

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
+  const [ updates, setUpdates ] = useState("")
   const params = useParams();
+  const history = useHistory();
 
   const fetchMovie = (id) => {
     axios
@@ -13,10 +15,33 @@ function Movie({ addToSavedList }) {
       .then((res) => setMovie(res.data))
       .catch((err) => console.log(err.response));
   };
-
+  
+  const updateMovie = (e) => {
+    e.preventDefault();
+    axios
+    .put(`http://localhost:5000/api/movies/${params.id}`, { ...movie, title: updates })
+    .then(res => {
+      console.log(res)
+      setMovie(res.data)
+    })
+    .catch(err => console.log(err.response))
+  }
+  
   const saveMovie = () => {
     addToSavedList(movie);
   };
+  
+  const deleteMovie = (e) => {
+    e.preventDefault();
+    axios
+    .delete(`http://localhost:5000/api/movies/${params.id}`)
+    .then(res => {
+      console.log(res)
+      history.push("/")
+      getMovieList()
+    })
+    .catch(err => console.log(err))
+  }
 
   useEffect(() => {
     fetchMovie(params.id);
@@ -30,9 +55,15 @@ function Movie({ addToSavedList }) {
     <div className="save-wrapper">
       <MovieCard movie={movie} />
 
-      <div className="save-button" onClick={saveMovie}>
-        Save
-      </div>
+      <div className="save-button" onClick={saveMovie}> Save </div>
+      <form onSubmit={updateMovie}>
+        <h2>Update movie title</h2>
+        <input type="text" onChange={e => setUpdates(e.target.value)} value={updates}/>
+        <button>Submit</button>
+      </form>
+      <form onSubmit={deleteMovie}>
+        <button>Delete Movie</button>
+      </form>
     </div>
   );
 }
