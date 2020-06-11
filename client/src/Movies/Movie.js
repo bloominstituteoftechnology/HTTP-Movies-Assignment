43c1277b63832c0bc7fdeae2d8ab0805bcaf4c3e
Oscar-Lopez-Projects <d1lopeztv@gmail.com>
2020-06-11 16:53:34 -0700

    finished the delete button, mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
+import {Link} from 'react-router-dom';
 
-function Movie({ addToSavedList }) {
-  const [movie, setMovie] = useState(null);
+function Movie({ addToSavedList}) {
+  const [movie, setMovie] = useState();
   const params = useParams();
+  const { push } = useHistory;
 
   const fetchMovie = (id) => {
     axios
@@ -23,18 +25,28 @@ function Movie({ addToSavedList }) {
   }, [params.id]);
 
   if (!movie) {
-    return <div>Loading movie information...</div>;
+    return <div>The data has been deleted......</div>;
   }
 
   return (
     <div className="save-wrapper">
       <MovieCard movie={movie} />
 
-      
-
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+
+      <Link to={`/`} 
+        onClick={()=>{
+          axios.delete(`http://localhost:5000/api/movies/${params.id}`, movie)
+          .then((res) => {
+            setMovie(...movie, res.data) 
+            push("/movies")
+          })
+          .catch((err) => console.log(err))
+          }}>
+      <div className="delete-button">delete </div>
+      </Link>
     </div>
   );
 }
