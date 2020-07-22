7800121ed338bb489eca6375bb0a722a280b2c1a
JM <manzur-1990@hotmail.com>
2020-07-22 15:53:06 -0500

    initial file set up

--- client/src/App.js
+++ client/src/App.js
@@ -35,6 +35,8 @@ const App = () => {
       <Route path="/movies/:id">
         <Movie addToSavedList={addToSavedList} />
       </Route>
+      
+      {/* <Rou path={"/update-movie/:id"} */}
     </>
   );
 };
