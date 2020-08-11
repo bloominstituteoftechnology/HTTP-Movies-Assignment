d19c38d2e8feb5a02fc2ed7e70a68f538b27b6a8
JenVest2020 <jenvest2019@gmail.com>
2020-08-10 22:23:54 -0500

    route added& form skeleton made

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import axios from 'axios';
+import UpdateForm from "./Movies/UpdateForm";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -34,6 +35,9 @@ const App = () => {
 
       <Route path="/movies/:id">
         <Movie addToSavedList={addToSavedList} />
+      </Route>
+      <Route path='/update-movie/:id'>
+        <UpdateForm />
       </Route>
     </>
   );
---
+++ client/src/Movies/UpdateForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UpdateForm = props => {
+
+    return (
+        <>
+
+        </>
+    )
+};
+
+export default UpdateForm;
