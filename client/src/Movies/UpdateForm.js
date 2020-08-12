1665fbd0ce263c93ae6d25ee3720664187da86d3
JenVest2020 <jenvest2019@gmail.com>
2020-08-12 14:42:27 -0500

    working use params issue for id pull

--- client/src/Movies/UpdateForm.js
+++ client/src/Movies/UpdateForm.js
@@ -1,38 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Axios from 'axios';
+
+const initialChange = {
+    title: '',
+    director: '',
+    metascore: '',
+    stars: '',
+};
 
 const UpdateForm = props => {
     console.log('from UpdateForm: props', props);
+    const [change, setChange] = useState(initialChange);
+    const { id } = useParams();
+    console.log('from updateForm: useParams', id);
+
+    useEffect(() => {
+        Axios.get(`http://localhost:5000/api/movies/${id}`)
+            .then(res => {
+                setChange(res.data);
+                console.log('UpdateForm: movies/id: res', res)
+            })
+            .catch(err => console.error('UpdateForm: movies/id failed: err', err.message))
+    }, [])
+
+
+
+    const handleChanges = e => {
+        setChange({
+            [e.target.name]: e.target.value
+        })
+
+    }
+
+
 
     return (
         <form className='update-form'>
             <label htmlFor='title' className='label'>Title</label>
-            <input 
+            <input
                 className='input'
                 type='text'
                 name='title'
                 value=''
-                placeholder='Title' />
+                placeholder='Title'
+                onChange={handleChanges} />
             <label htmlFor='director' className='label'>Director</label>
-            <input 
+            <input
                 className='input'
                 type='text'
                 name='director'
                 value=''
-                placeholder='Director' />
+                placeholder='Director'
+                onChange={handleChanges} />
             <label htmlFor='metascore' className='label'>Meta Score</label>
-            <input 
+            <input
                 className='input'
                 type='number'
                 name='metascore'
                 value=''
-                placeholder='Metascore' />
+                placeholder='Metascore'
+                onChange={handleChanges} />
             <label htmlFor='stars' className='label'>Stars</label>
-            <input 
-                className= 'input'
+            <input
+                className='input'
                 type='text'
                 name='stars'
                 value=''
-                placeholder='Stars' />
+                placeholder='Stars'
+                onChange={handleChanges} />
             <button className='edit-movie-btn'>Submit Edit</button>
         </form>
     )
