09e21d4616c4a26276b1ff127cdfcb2acc2d7b50
sarahmarie1976 <sholle7@gmail.com>
2020-08-12 22:08:33 -0500

    :tada: did some stretch

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
+import { Navbar } from 'reactstrap';
 
 function SavedList({ list }) {
 	return (
+    <Navbar style={{ width: '70%', marginLeft: '15%' }} >
 		<div className="saved-list">
 			<h3>Saved Movies:</h3>
 			{list.map((movie) => {
 				return (
+          
 					<NavLink
 						to={`/movies/${movie.id}`}
 						key={movie.id}
@@ -22,6 +26,7 @@ function SavedList({ list }) {
 				<Link to="/add-movie">Add Movie</Link>
 			</div>
 		</div>
+    </Navbar>
 	);
 }
 
