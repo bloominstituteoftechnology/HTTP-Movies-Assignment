adf4e38c353d67f0253900d0f8a69113476080d8
W-B28 <wtb2804@gmail.com>
2020-08-20 19:50:05 -0500

    AddMovie Compompoent

--- client/src/components/AddForm.jsx
+++ client/src/components/AddForm.jsx
@@ -2,41 +2,67 @@ import React, { useState } from 'react';
 import axios from "axios";
 
 
-function AddForm(props) {
+function AddMovie(props) {
     console.log(props)
-    const [ movie, setMovie ] = useState({
+    const { add, setAdd} = useState({
         id: "",
         title: "",
         metascore: "",
-        stars: []
+        stars: ""
     })
    
-     const hangleChange = e => {
+      hangleChange = e => {
         setMovie({
-            ...movie, 
+            ...add, 
             [e.target.name]: e.target.value
         });
     }
 
-     const handleSubmit = e => {
+      handleSubmit = e => {
         e.preventDefault();
         axios
             .post(`http://localhost/api/movies`, add)
             .then(res => {props.history.push('/')})
             .catch( err => console.log(err)) 
-                // alert("Whoops our bad, an error occured :/")
-    }
+        }  
     
   
 
     return (
         <div> 
             <form onSubmt={handleSubmit}> 
-            <h2><props.movie.id</h2>
-                 
-            </form>
-        </div>
-    )
-}
+            <h1>{add.title}</h1>
+      <p>{add.director}</p>
+      <p>{add.metascore}</p>
+      <p>{add.stars}</p>
+      <input
+        name='title'
+        placeholder='Title'
+        value={add.title}
+        onChange={handleChange}
+      />
+      <input
+        name='director'
+        placeholder='Director'
+        value={add.director}
+        onChange={handleChange}
+      />
+      <input
+        name='metascore'
+        placeholder='Metascore'
+        value={add.metascore}
+        onChange={handleChange}
+      />
+      <input
+        name='stars'
+        placeholder='Stars'
+        value={add.stars}
+        onChange={handleChange}
+      />
+    </form>
+</div>
+  );
+};
 
-export default AddForm;
+export default AddMovie;
+                 
