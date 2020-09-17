c5569c413e0674941d954cf4d89b328a74c0306b
unknown <olem2233@gmail.com>
2020-09-16 21:54:41 -0700

    MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
-function Movie({ addToSavedList }) {
+function Movie({ addToSavedList, refreshHandler }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const { push } = useHistory()
 
   const fetchMovie = (id) => {
     axios
@@ -17,6 +18,19 @@ function Movie({ addToSavedList }) {
   const saveMovie = () => {
     addToSavedList(movie);
   };
+
+  const deleteMovie = (e, id) => {
+    e.preventDefault()
+    axios
+      .delete(`http://localhost:5000/api/movies/${id}`)
+      .then(res => {
+        push(`/movies`)
+      })
+      .catch(err => console.log(err))
+      .finally(() => {
+        refreshHandler()
+      })
+  }
 
   useEffect(() => {
     fetchMovie(params.id);
@@ -33,6 +47,8 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <button className="edit-button" onClick={() => push(`/update-movie/${movie.id}`)}>Edit</button>
+      <button onClick={ (e) => deleteMovie(e, params.id)}>Delete Movie</button>
     </div>
   );
 }
