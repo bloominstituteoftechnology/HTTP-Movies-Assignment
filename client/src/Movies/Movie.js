40c21167131260d259b2eee03f3747033b12b0e5
AJ Gambino <aj.gambino7@gmail.com>
2020-09-17 16:46:34 -0400

    fixed branch issue

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -6,7 +6,7 @@ import MovieCard from "./MovieCard";
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
-
+ 
   const fetchMovie = (id) => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
