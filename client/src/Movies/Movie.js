9215dd9387c56591de95659d89d7d4e28442415e
AJ Gambino <aj.gambino7@gmail.com>
2020-09-17 16:43:15 -0400

    switched computers

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import axios from "axios"; 
 import { useParams } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
