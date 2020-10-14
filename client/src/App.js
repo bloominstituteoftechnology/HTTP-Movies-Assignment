f930efbeb7cc5cadb2f8653624dae4d18682fba9
willwearing <willwearing@gmail.com>
2020-10-14 12:51:17 -0600

    put and delete are done

--- client/src/App.js
+++ client/src/App.js
@@ -3,40 +3,37 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
-import axios from 'axios';
-
+import axios from "axios";
+import UpdateForm from "./Movies/UpdateForm";
 const App = () => {
   const [savedList, setSavedList] = useState([]);
   const [movieList, setMovieList] = useState([]);
-
   const getMovieList = () => {
     axios
       .get("http://localhost:5000/api/movies")
-      .then(res => setMovieList(res.data))
-      .catch(err => console.log(err.response));
+      .then((res) => setMovieList(res.data))
+      .catch((err) => console.log(err.response));
   };
-
-  const addToSavedList = movie => {
+  const addToSavedList = (movie) => {
     setSavedList([...savedList, movie]);
   };
-
   useEffect(() => {
     getMovieList();
-  }, []);
+  }, [movieList]);
 
   return (
     <>
       <SavedList list={savedList} />
-
       <Route exact path="/">
         <MovieList movies={movieList} />
       </Route>
-
+      <Route path="/update-movie/:id">
+        <UpdateForm movieList={movieList} setMovieList={setMovieList} />
+      </Route>
       <Route path="/movies/:id">
         <Movie addToSavedList={addToSavedList} />
       </Route>
     </>
   );
 };
-
 export default App;
