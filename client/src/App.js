b4a0647bc5b5ed77bfba6c41937ed2d8f1f6da49
Ahmad Wali Sayadi <ahmadwalisayadi@Ahmads-MacBook-Pro.local>
2021-01-19 22:47:38 -0800

    project comepleted

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
+import UpdateMovie from "./Movies/MovieForm";
 import Movie from "./Movies/Movie";
 import axios from 'axios';
 
-const App = () => {
+const App = (props) => {
+  console.log('app', props)
   const [savedList, setSavedList] = useState([]);
   const [movieList, setMovieList] = useState([]);
 
@@ -16,8 +18,15 @@ const App = () => {
       .catch(err => console.log(err.response));
   };
 
-  const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
+  const addToSavedList = movie => { 
+    let xx = savedList.map(savedInList => {
+      return savedInList.title
+    })
+    if (xx.includes(movie.title)) {
+      alert('cant add 2 movies of the same title')
+    } else {
+      setSavedList([...savedList, movie])
+    }    
   };
 
   useEffect(() => {
@@ -32,11 +41,20 @@ const App = () => {
         <MovieList movies={movieList} />
       </Route>
 
-      <Route path="/movies/:id">
+      <Route 
+        path="/movies/:id"
+        render={props => <Movie {...props} addToSavedList={addToSavedList} />}
+        >
         <Movie addToSavedList={addToSavedList} />
       </Route>
+      
+      <Route
+        path={`/update-movie/:id`}
+        render={props => <UpdateMovie {...props} movies={movieList} />}
+      />
     </>
   );
 };
 
 export default App;
+
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, Route, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const history = useHistory();
 
   const fetchMovie = (id) => {
     axios
@@ -16,6 +17,17 @@ function Movie({ addToSavedList }) {
 
   const saveMovie = () => {
     addToSavedList(movie);
+  };
+
+  const deleteMovie = (e) => {
+    e.preventDefault();
+
+    axios.delete(`http://localhost:5000/api/movies/${movie.id}`)
+      .then(res => {
+        console.log(res)
+        history.push("/");
+      })
+      .finally(() => window.location.reload())
   };
 
   useEffect(() => {
@@ -33,6 +45,20 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <div className="edit-button" 
+        onClick={() => {
+          history.push(`/update-movie/${movie.id}`);
+        }}
+      >
+        Update
+      </div> 
+      <div className="delete-button"
+        onClick={deleteMovie}
+      >
+        Delete
+      </div>
+
+      
     </div>
   );
 }
