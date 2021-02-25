f2915490bb3b1b5f9aebffdaa36e9918fcd45070
George Hatzigeorgio <geogehatzigeorgio@gmail.com>
2021-02-25 06:44:38 -0500

    commit

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams , useHistory} from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
-
+  const {push} = useHistory()
   const fetchMovie = (id) => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
@@ -25,13 +25,19 @@ function Movie({ addToSavedList }) {
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
+  const handleClick = (id) =>{
+    push(`update-movie/${params.id}`)
 
+  }
   return (
     <div className="save-wrapper">
       <MovieCard movie={movie} />
 
       <div className="save-button" onClick={saveMovie}>
         Save
+      </div>
+      <div>
+          <button onclick={handleClick} >UpdateMovie</button>
       </div>
     </div>
   );
---
+++ client/src/UpdateMovie.js
@@ -0,0 +1,85 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+
+const initialMovie = {
+	id: Date.now(),
+	title: "",
+	director: "",
+	metascore: "",
+	stars: [],
+};
+
+export default function UpdateMovie(props) {
+	const [movie, setMovie] = useState(initialMovie);
+
+	const { id } = useParams();
+	const { push } = useHistory();
+
+	const handleChange = (e) => {
+		e.persist();
+		setMovie({
+			...movie,
+			[e.target.name]: e.target.value,
+		});
+	};
+	const onSubmit = (e) => {
+		e.preventDefault();
+		axios
+			.put(`http:localhost:5000/api/movies/${id}`, movie)
+			.then((res) => {
+				props.setMovieList(res.data);
+				console.log(res.data);
+				setMovie({ ...movie });
+				push("/movies");
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	return (
+		<div>
+			<h2>Update Movie</h2>
+			<form onSubmit={onSubmit}>
+				<input
+					type="text"
+					name="title"
+					onChange={handleChange}
+					placeholder="title"
+					value={movie.title}
+				/>
+				<div className="baseline" />
+
+				<input
+					type="text"
+					name="director"
+					onChange={handleChange}
+					placeholder="Director"
+					value={movie.director}
+				/>
+				<div className="baseline" />
+
+				<input
+					type="number"
+					name="metascore"
+					onChange={handleChange}
+					placeholder="metascore"
+					value={movie.metascore}
+				/>
+				<div className="baseline" />
+
+				<div className="baseline" />
+
+				<input
+					type="string"
+					name="stars"
+					onChange={handleChange}
+					placeholder="stars"
+					value={movie.stars}
+				/>
+				<div className="baseline" />
+			</form>
+		</div>
+	);
+}
